95f9ce5eab584511a25d8951701b94379b576883
anushka-cloduraai <63954873+anushka192001@users.noreply.github.com>
2024-06-04 20:24:54 +0530

    committed

---
+++ app.py
@@ -0,0 +1,23 @@
+import streamlit as st
+import numpy as np
+import transformers
+import torch
+from torch.utils.data import Dataset, DataLoader, RandomSampler, SequentialSampler
+from transformers import DistilBertTokenizer, DistilBertModel
+import import_ipynb
+import new  
+
+
+#@st.cache(allow_output_mutation=True)
+def functional_level_prediction(text):
+   result = new.find_functional_label(text)
+   return result
+
+user_input = st.text_area('enter profile description to get its functional level')
+print(type(user_input))
+button = st.button('predict')
+
+if user_input and button:
+    print(type(user_input))
+    result = functional_level_prediction(user_input) 
+    st.write('functional_level: ',result)
